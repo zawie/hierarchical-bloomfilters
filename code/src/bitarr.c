617ddda4cf03ef4a64daecccad2c9773caa33685
zawie <zawie@rice.edu>
2022-09-14 12:16:56 -0500

    initial bit array attempt

---
+++ code/src/bitarr.c
@@ -0,0 +1,31 @@
+#include "bitarr.h";
+
+bitarr_t * bitarr_init(unsigned num_bits) {
+    bitarr_t *b = (bitarr_t *) malloc(sizeof(bitarr_t));
+    b->num_bits = num_bits;
+    //valloc manually allocates memory starting at a page boundary (https://ftp.gnu.org/old-gnu/Manuals/glibc-2.2.3/html_chapter/libc_3.html)
+    b->arr = (arr_t *) valloc((num_bits/BITS_PER_BYTE));
+}
+
+void bitarr_free(bitarr_t * bitarr) {
+    free(bitarr->arr);
+    free(bitarr);
+}
+
+void bitarr_set(bitarr_t * bitarr, unsigned index) {
+    unsigned section_index = index/BITS_PER_ARR_T;
+    unsigned bit_index = index % BITS_PER_ARR_T;
+    bitarr->arr[section_index] |= (1 >> bit_index);
+}
+
+void bitarr_unset(bitarr_t * bitarr, unsigned index) {
+    unsigned section_index = index/BITS_PER_ARR_T;
+    unsigned bit_index = index % BITS_PER_ARR_T;
+    bitarr->arr[section_index] ^= (1 >> bit_index);
+}
+
+bool bitarr_check(bitarr_t * bitarr, unsigned index) {
+    unsigned section_index = index/BITS_PER_ARR_T;
+    unsigned bit_index = index % BITS_PER_ARR_T;
+    return (bool) bitarr->arr[section_index] & (1 >> bit_index);
+}
---
+++ code/src/bitarr.h
@@ -0,0 +1,59 @@
+#ifndef _BITARR_H
+#define _BITARR_H
+
+#include<stdio.h>
+#include <stdbool.h>
+
+#define arr_t           size_t
+#define BITS_PER_BYTE   8
+#define BYTES_PER_ARR_T (sizeof(arr_t))
+#define BITS_PER_ARR_T  (BITS_PER_BYTE*BYTES_PER_ARR_T)
+
+#define IS_VALID_INDEX (bitarr, index) ((bool) (((unsigned) index) < bitarr->num_bits)) //Used to check if an index is valid
+
+typedef struct bit_arry {
+    unsigned num_bits;
+    arr_t* arr;
+} bitarr_t;
+
+/**
+ * Intializes a bit array of a specified size starting at a page boundary.
+ * 
+ * @param num_bits the number of bits desired in the bit array
+ * @return a struct pointer representing the data structure
+ */
+bitarr_t * bitarr_init(unsigned num_bits);
+
+/**
+ * Frees all memory allocated to create the bit array
+ * 
+ * @param bitarr the bit array to free
+ */
+void bitarr_free(bitarr_t * bitarr);
+
+/**
+ * Sets the bit at the specified index.
+ * 
+ * @param bitarr the bit array to set
+ * @param index the bit to set
+ */
+void bitarr_set(bitarr_t * bitarr, unsigned index);
+
+/**
+ * Unsets the bit at the specified index.
+ * 
+ * @param bitarr the bit array to modify
+ * @param index the bit to unset
+ */
+void bitarr_unset(bitarr_t * bitarr, unsigned index);
+
+
+/**
+ * Checks whether the bit at the specified index is set.
+ * 
+ * @param bitarr the bit array to modify
+ * @param index the bit to check
+ */
+bool bitarr_check(bitarr_t * bitarr, unsigned index);
+
+#endif
