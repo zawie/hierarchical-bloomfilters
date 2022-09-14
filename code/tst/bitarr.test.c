f9cfd831dcb851c0f4137adac7bcb8ffb0670b38
zawie <zawie@rice.edu>
2022-09-14 14:40:32 -0500

    unset set

--- code/tst/bitarr.test.c
+++ code/tst/bitarr.test.c
@@ -13,7 +13,7 @@ void tearDown(void)
 
 }
 
-void test_set(void)
+void test_set_evens(void)
 {
     int i;
     bitarr_t * b = bitarr_init(NUM_BITS);
@@ -24,25 +24,48 @@ void test_set(void)
     }
 
     /* Ensure all bits are set as expected */
+    byte_t expected = 0b01010101;
     //Check bytes that should be fully set
     for(i = 0; i < b->size - 1; i++) {
-      TEST_ASSERT_BITS(0xFF, 0x55, b->arr[i]);
+      TEST_ASSERT_BITS(0xFF, expected, b->arr[i]);
     }
     //Check last byte, which could be partially set
-    TEST_ASSERT_BITS(0xFF >> (BITS_PER_BYTE - (NUM_BITS % BITS_PER_BYTE)), 0x55, b->arr[i]);
+    TEST_ASSERT_BITS(0xFF >> (BITS_PER_BYTE - (NUM_BITS % BITS_PER_BYTE)), expected, b->arr[i]);
 
     bitarr_free(b);
 }
 
-// void test_unset(void)
-// {
-//   TEST_ASSERT_EQUAL_HEX8(80,80);
-// }
+void test_unset(void)
+{
+  int i;
+  bitarr_t * b = bitarr_init(NUM_BITS);
+
+  /* Set all even bits */
+  for(i = 0; i < NUM_BITS; i += 2) {
+    bitarr_set(b, i);
+  }
+
+  /* Unset every fourth bits */
+  for(i = 0; i < NUM_BITS; i += 4) {
+    bitarr_unset(b, i);
+  }
+
+  /* Ensure all bits are set as expected */
+  //Check bytes that should be fully set
+  byte_t expected = 0b01000100;
+  for(i = 0; i < b->size - 1; i++) {
+    TEST_ASSERT_BITS(0xFF, expected, b->arr[i]);
+  }
+  //Check last byte, which could be partially set
+  TEST_ASSERT_BITS(0xFF >> (BITS_PER_BYTE - (NUM_BITS % BITS_PER_BYTE)), expected, b->arr[i]);
+
+  bitarr_free(b);
+}
 
 int main(void)
 {
     UNITY_BEGIN();
-    RUN_TEST(test_set);
-    // RUN_TEST(test_AverageThreeBytes_should_AverageHighValues);
+    RUN_TEST(test_set_evens);
+    RUN_TEST(test_unset);
     return UNITY_END();
 }
