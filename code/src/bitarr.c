919495bf2a75c9637417d7dd7fbcab410a13eed3
zawie <zawie@rice.edu>
2022-09-14 14:40:27 -0500

    cleaner bitarr ops

--- code/src/bitarr.c
+++ code/src/bitarr.c
@@ -1,5 +1,8 @@
 #include "bitarr.h"
 
+#define GET_BYTE_INDEX(index) (index/BITS_PER_BYTE)
+#define GET_BIT_INDEX(index) (index % BITS_PER_BYTE)
+
 bitarr_t * bitarr_init(unsigned num_bits) {
     bitarr_t *b = (bitarr_t *) malloc(sizeof(bitarr_t));
     b->num_bits = num_bits;
@@ -15,19 +18,13 @@ void bitarr_free(bitarr_t * bitarr) {
 }
 
 void bitarr_set(bitarr_t * bitarr, unsigned index) {
-    unsigned section_index = index/BITS_PER_BYTE;
-    unsigned bit_index = index % BITS_PER_BYTE;
-    bitarr->arr[section_index] |= (1 << bit_index);
+    bitarr->arr[GET_BYTE_INDEX(index)] |= (1 << GET_BIT_INDEX(index));
 }
 
 void bitarr_unset(bitarr_t * bitarr, unsigned index) {
-    unsigned section_index = index/BITS_PER_BYTE;
-    unsigned bit_index = index % BITS_PER_BYTE;
-    bitarr->arr[section_index] &= ~(1 << bit_index);
+    bitarr->arr[GET_BYTE_INDEX(index)] &= ~(1 << GET_BIT_INDEX(index));
 }
 
 bool bitarr_check(bitarr_t * bitarr, unsigned index) {
-    unsigned section_index = index/BITS_PER_BYTE;
-    unsigned bit_index = index % BITS_PER_BYTE;
-    return (bool) bitarr->arr[section_index] & (1 >> bit_index);
+    return (bool) bitarr->arr[GET_BYTE_INDEX(index)] & (1 << GET_BIT_INDEX(index));
 }
