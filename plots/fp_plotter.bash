d1ba342d6c30b0f25882e2de7d994ae8b5c1b88e
zawie <zawie@rice.edu>
2022-12-06 15:22:39 -0600

    plots theoretical expectation

--- plots/fp_plotter.bash
+++ plots/fp_plotter.bash
@@ -15,7 +15,12 @@ gnuplot -persist <<-EOFMarker
 
     set style line 1 lw 1 lc rgb '#0000FF' pt 6 lt 5
     set style line 2 lw 1 lc rgb '#FF0000' pt 6 lt 5
+    set style line 3 lw 1 lc rgb '#A020F0' pt 6 lt 5
+
+    k = 7
+    t(x) = (1 - exp(-k/x))**k
 
     plot "$1" using 1:2 title 'Standard' ls 1,\
-         "$1" using 1:3 title 'Hierarchial' ls 2 
+         "$1" using 1:3 title 'Hierarchial' ls 2,\
+         t(x) title 'Theoretical' ls 3;                         ls 3
 EOFMarker
